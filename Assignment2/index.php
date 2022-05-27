3de1655a8ec37f265b2e154d2fff12d4cf31212e
asimmakhmudov <asimmahmudov18@gmail.com>
2022-05-27 04:59:57 +0400

    assignments

---
+++ Assignment2/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    @font-face {
+        font-family: clock;
+        src: url(digital-7.ttf);
+    }
+
+    body {
+        background-color: #242424;
+        color: #fff;
+        font-family: clock;
+        font-size: 50px;
+        text-align: center;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    .container {
+        width: 550px;
+        height: 320px;
+        border-radius: 30px 30px 0 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: blue;
+        background: #c4c4c4;
+    }
+    .cont {
+        background: #16191E;
+        width: 800px;
+        height: 50px;
+    }
+    #time {
+        font-family: clock;
+        font-size: 60px;
+        width: 400px;
+        height: 200px;
+        border: 1px solid gray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #1c1c1c;
+    }
+</style>
+
+<body>
+    <script>
+        $(document).ready(function() {
+            setInterval(function() {
+                $("#time").load("time.php");
+            }, 1000);
+        });
+    </script>
+
+    <div class="container">
+        <div id="time">
+            00 : 00 : 00 PM
+        </div>
+    </div>
+    <div class="cont"></div>
+
+</body>
+
+</html>
